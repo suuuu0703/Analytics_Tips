3ff85952f1316c00fb352c9e1baf52e0020dbfc2
suuuu0703 <takayuki.suzuki.0703@gmail.com>
2021-02-22 14:35:30 +0900

    Update 指定期間の日付列の生成.sql

--- Tresuredata_SQL_presto/指定期間の日付列の生成.sql
+++ Tresuredata_SQL_presto/指定期間の日付列の生成.sql
@@ -1,6 +1,7 @@
 select
     td_time_format(date_col, 'yyyy-MM-dd', 'JST') as base_date
     ,td_time_format(td_time_parse(cast(date_trunc('week', cast(td_time_format(date_col, 'yyyy-MM-dd HH:MM:SS', 'JST') as timestamp)) as varchar)), 'yyyy-MM-dd') as base_week
+    ,td_time_format(date_col, 'yyyy-MM-01', 'JST') as base_month
 from (
     select
         d
